bfb338bf4a8f1a2c1f961836f63461d1b3419c1c
Thang <lhuthng@gmail.com>
2022-02-07 16:15:37 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Phaser from 'phaser';
+import { useState, useEffect } from 'react';
+
+const game = {
+  width: "100%",
+  height: "100%",
+  type: Phaser.AUTO,
+  scene: {
+    init: function() {
+      this.cameras.main.setBackgroundColor('#24252A')
+    },
+    create: function() {
+      this.helloWorld = this.add.text(
+        this.cameras.main.centerX, 
+        this.cameras.main.centerY, 
+        "Hello World", { 
+          font: "40px Arial", 
+          fill: "#ffffff" 
+        }
+      );
+      this.helloWorld.setOrigin(0.5);
+    },
+    update: function() {
+      this.helloWorld.angle += 1;
+    }
+  }
+}
 
 function App() {
+  const [state, setState] = useState({});
+  useEffect(() => {
+    
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IonPhaser game={game} />
   );
 }
 
